283d15aa89c2242129d94cbc8041dcd5d97ab2b4
Tugume Andrew <andrewtugume2@gmail.com>
2020-08-26 12:22:17 +0300

    update

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 var router = express.Router();
 
+
 //app.METHOD(PATH,HANDLER);
 router.get('/',function(req,res){
     res.send('All users');
 })
 
-router.get('/women',function(req,res){
-    res.send('Women Users');
+router.get('/url',function(req,res){
+    res.json(["Tony","Edna","Elwin","xindana"]);
 })
 
 router.get('/men',function(req,res){
